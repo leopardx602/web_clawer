c5589c6c0a7cd8196bfcb6e1954f24f963ac8cbc
chen <leopardx602@gmail.com>
2021-11-12 18:15:14 +0800

    update

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/gocolly/colly"
@@ -17,14 +18,14 @@ type Product struct {
 	ProductURL string
 }
 
-func main() {
-	jobs := make(chan string, 5)
-	finishJobs := make(chan int, 5)
-	products := make(chan Product, 100)
+type Yahoo struct {
+	c       *colly.Collector
+	jobs    chan string
+	keyword string
+}
 
-	c := colly.NewCollector()
-
-	c.OnHTML(".BaseGridItem__grid___2wuJ7", func(e *colly.HTMLElement) {
+func (y *Yahoo) Parse(products chan Product) {
+	y.c.OnHTML(".BaseGridItem__grid___2wuJ7", func(e *colly.HTMLElement) {
 		var product Product
 		product.Name = e.DOM.Find(".BaseGridItem__title___2HWui").Text()
 		product.ImageURL, _ = e.DOM.Find(".SquareImg_img_2gAcq").Attr("src")
@@ -47,40 +48,83 @@ func main() {
 		product.Price = price
 		products <- product
 	})
+}
 
-	c.OnError(func(_ *colly.Response, err error) {
+func (y *Yahoo) CreateJobs() {
+	// Create the jobs
+	for i := 0; i < 5; i++ {
+		y.jobs <- fmt.Sprintf("https://tw.buy.yahoo.com/search/product?p=%s&pg=%v", y.keyword, i)
+	}
+	close(y.jobs)
+}
+
+func Clawer(yahoo *Yahoo, products chan Product, wg *sync.WaitGroup) {
+	finishJobs := make(chan int, 5)
+
+	yahoo.Parse(products) // different
+	yahoo.c.OnError(func(_ *colly.Response, err error) {
 		log.Println("Something went wrong:", err)
 	})
 
-	c.OnScraped(func(r *colly.Response) {
+	yahoo.c.OnScraped(func(r *colly.Response) {
 		fmt.Println("Finished", r.Request.URL)
 		finishJobs <- 1
 		if len(finishJobs) == 5 {
 			time.Sleep(time.Second)
-			close(products)
+			wg.Done()
 		}
 	})
 
 	// Assign the jobs to the workers
 	worker := func(id int, jobs <-chan string) {
 		for url := range jobs {
-			c.Visit(url)
+			yahoo.c.Visit(url)
 			time.Sleep(1 * time.Second)
 		}
 	}
 	for w := 1; w <= 3; w++ {
-		go worker(w, jobs)
+		go worker(w, yahoo.jobs)
 	}
 
 	// Create the jobs
-	for i := 0; i < 5; i++ {
-		jobs <- fmt.Sprintf("https://tw.buy.yahoo.com/search/product?p=iphone&pg=%v", i)
-	}
-	close(jobs)
+	yahoo.CreateJobs() // different
+}
 
-	// Result
-	for product := range products {
-		fmt.Println(product)
-	}
+// type Pchome struct {
+// 	c       *colly.Collector
+// 	jobs    chan string
+// 	keyword string
+// }
+
+// func (y *Pchome) Parse(products chan Product) {
+// 	fmt.Println("pchome parse")
+// }
+
+// func (y *Pchome) CreateJobs() {
+// 	fmt.Println("create jobs")
+// }
+
+func WebClawer(keyword string) {
+	products := make(chan Product, 100)
+
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+
+	yahoo := Yahoo{c: colly.NewCollector(), keyword: keyword, jobs: make(chan string, 5)}
+	go Clawer(&yahoo, products, wg)
+
+	// Output
+	go func() {
+		for product := range products {
+			fmt.Println(product)
+		}
+	}()
+
+	wg.Wait()
+	close(products)
 	fmt.Println("Done")
+}
+
+func main() {
+	WebClawer("iphone")
 }
